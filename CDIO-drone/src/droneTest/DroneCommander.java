b7f7f8b7a97031c4e93a2c13f97d77de85dab201
SimonChr <simonchristiansen1@gmail.com>
2018-06-14 13:35:21 +0200

    Comments added

--- CDIO-drone/src/droneTest/DroneCommander.java
+++ CDIO-drone/src/droneTest/DroneCommander.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Author: Simon Christiansen
  */
 
@@ -30,56 +30,88 @@ public class DroneCommander implements CircleListener{
 	private DroneAlttitudeListener nav;
 	private droneGUI dronegui = null;
 	
-	private int speed = 30;
-	private int slowspeed = 5;
-	private int slowtime = 50;
-	private double ErrorMargin = 35;
+	private int speed = 30;  // The base velocity in %
+	private int slowspeed = 5;  //  The velocity used for centralizing in %
+	private int slowtime = 50;	//	The time centralizing commands are done in ms
+	private double ErrorMargin = 35;	// the Margin of Error in which the center of a circle can be fount
 	
-	private int count = 0;
-	private int methodecount = 0;
-	private int countmax = 6;
-	private boolean findCircle = true;
+	private int count = 0;	//	The counter for amount of centered circles 
+	private int methodecount = 0;  // Count Used to limit the calls done in findCircleCenter
+	private int countmax = 6;	//	The amount of centered circles we need before flying through a circle
+	private boolean findCircle = true;	//	When true we look for circles
 	
-	private double midPoint_x = GUITest.IMAGE_WIDTH/2;
-	private double midPoint_y = GUITest.IMAGE_HEIGHT/2;
+	private double midPoint_x = GUITest.IMAGE_WIDTH/2;	// Camera midpoint in x
+	private double midPoint_y = GUITest.IMAGE_HEIGHT/2;	// camera midpoint in y
 
-	private double max_radius = 140;
+	private double max_radius = 140;	// The size of the circle we want on the camera
 	
 	private Circle[] circles;
 	
-		
+		/**
+		 * The DroneCommander Constructor
+		 * @param drone
+		 * @param speed
+		 * @param gui
+		 */
 	public DroneCommander(ARDrone drone, int speed, droneGUI gui){
 		this.drone = drone;
 		this.speed = speed;	
 		this.dronegui = gui;
 		cmd = drone.getCommandManager();
 		nav = new DroneAlttitudeListener(drone);
-		circles = dronegui.circles;
 	}
 	
+	/**
+	 * Take off Method
+	 * The first method called when you need to fly
+	 */
 	public void takeOff(){
 		cmd.takeOff().doFor(5000);
 		System.out.println("Takeoff");
 	}
 	
+	/**
+	 * Hover Method
+	 * Makes the drone stand still in place
+	 * with 1000 given as the amount of time in ms
+	 */
 	public void hover(){
-		cmd.hover().doFor(5000);
 		cmd.hover().doFor(1000);
 	}
 	
+	/**
+	 * Hover Method
+	 * @param ms
+	 */
 	public void hover(int ms){
 		cmd.hover().doFor(ms);
 	}
 	
+	/**
+	 * Landing Method
+	 * When the drone is done flying
+	 */
 	public void Landing(){
 		cmd.landing();
 		System.out.println("Landing");
 	}
 	
+	/**
+	 * Turn Left Method
+	 * Makes the drone spin left with speed given as the speed 
+	 * and the amount of time spinning given as a parameter in ms
+	 * @param ms
+	 */
 	public void TurnLeft(int ms){
 		cmd.spinLeft(speed).doFor(ms);
 	}
 	
+	/**
+	 * Right Left Method
+	 * Makes the drone spin right with speed given as the speed 
+	 * and the amount of time spinning given as a parameter in ms
+	 * @param ms
+	 */
 	public void TurnRight(int ms){
 		cmd.spinRight(speed).doFor(ms);
 	}
@@ -104,18 +136,34 @@ public class DroneCommander implements CircleListener{
 		cmd.spinRight(speed).doFor(ms);
 	}
 	
+	/**
+	 * Fly Right Method
+	 * makes the drone fly right with the given velocity of the variable speed
+	 */
 	public void flyRight(){
 		cmd.goRight(speed);
 	}
 	
+	/**
+	 * Fly Left Method
+	 * makes the drone fly left with the given velocity of the variable speed
+	 */
 	public void flyLeft(){
 		cmd.goLeft(speed);
 	}
 	
+	/**
+	 * Fly forward Method
+	 * makes the drone fly forward with the given velocity of the variable speed
+	 */
 	public void flyForward(){
 		cmd.forward(speed);
 	}
 	
+	/**
+	 * Fly backward Method
+	 * makes the drone fly backward with the given velocity of the variable speed
+	 */
 	public void flyBackward(){
 		cmd.backward(speed);
 	}
@@ -139,22 +187,40 @@ public class DroneCommander implements CircleListener{
 	}
 	
 	/**
-	 * Overloaded method, used to set speed of drone in %, and time it shall fly in ms
+	 * Fly right method
+	 * with time given as the slow time variable
+	 * used for findCircleCenter()
 	 * @param speed
-	 * @param ms
 	 */
 	public void flyRight(int speed){
 		cmd.goRight(speed).doFor(slowtime);
 	}
 	
+	/**
+	 * Fly left method
+	 * with time given as the slow time variable
+	 * used for findCircleCenter()
+	 * @param speed
+	 */
 	public void flyLeft(int speed){
 		cmd.goLeft(speed).doFor(slowtime);
 	}
 	
+	/**
+	 * Fly forward method
+	 * with time given as the slow time variable
+	 * used for findCircleCenter()
+	 * @param speed
+	 */
 	public void flyForward(int speed){
 		cmd.forward(speed).doFor(slowtime);
 	}
 	
+	/**
+	 * Overloaded method, used to set speed of drone in %, and time it shall fly in ms
+	 * @param speed
+	 * @param ms
+	 */
 	public void flyForward(int speed, int ms){
 		cmd.forward(speed).doFor(ms);
 	}
@@ -167,31 +233,66 @@ public class DroneCommander implements CircleListener{
 	public void flyBackward(int speed, int ms){
 		cmd.backward(speed).doFor(ms);
 	}
+	
+	/**
+	 * Fly Backward method
+	 * with time given as the slow time variable
+	 * used for findCircleCenter()
+	 * @param speed
+	 */
 	public void flyBackward(int speed){
 		cmd.backward(speed).doFor(slowtime);
 	}
 	
+	/**
+	 * Increase Altitude method
+	 * with time given as the slow time variable
+	 * used for findCircleCenter()
+	 * @param speed
+	 */
 	public void increaseAltitude(int speed){
-		cmd.up(speed).doFor(1000);
 		cmd.up(speed).doFor(slowtime);
 	}
 	
+	/**
+	 * Fly Backward method
+	 * @param speed
+	 * @param ms
+	 */
 	public void increaseAltitude(int speed, int ms){
 		cmd.up(speed).doFor(ms);
 	}
 	
+	/**
+	 * decrease altitude method
+	 * with time given as the slow time variable
+	 * used for findCircleCenter()
+	 * @param speed
+	 */
 	public void decreaseAltitude(int speed){
-		cmd.down(speed).doFor(1000);
 		cmd.down(speed).doFor(slowtime);
 	}
 	
+	/**
+	 * set findcircle method
+	 * set the findcircle variable to either true of false
+	 * set to true when wanting to find circle
+	 * set to false when you have flown through a circle
+	 * @param true_false
+	 */
 	public void setFindCircle(boolean true_false){
 		findCircle = true_false;
 	}
 	
+	/**
+	 * get find circle method
+	 * when you want to get the findcircle variable
+	 * @return
+	 */
 	public boolean getFindCircle(){
 		return findCircle;
 	}
+	
 	
 	public void moveToAltitude(int height){
 		while(true){
@@ -210,6 +311,13 @@ public class DroneCommander implements CircleListener{
 		}
 	}
 	
+	/**
+	 * Circle in center
+	 * A boolean method used to see if the drone have found the circles center
+	 * within the margin of error
+	 * @param circle
+	 * @return
+	 */
 	public boolean CircleInCenter(Circle circle){
 		boolean inCenter = false;
 				
@@ -223,6 +331,14 @@ public class DroneCommander implements CircleListener{
 		return inCenter;
 	}
 	
+	/**
+	 * Find Circle Center Method
+	 * This method find the center of a circle
+	 * to limit the amount of commands the methodecounter is used
+	 * the thread sleeps for and given time in ms every 4th call of this method
+	 * also to limit amount of commands called
+	 * @param circle
+	 */
 	public void findCircleCenter(Circle circle) {
 		
 		double circle_x = circle.x;
@@ -276,10 +392,14 @@ public class DroneCommander implements CircleListener{
 		}
 	}
 	
+	/**
+	 * Fly Through circle method
+	 * This method calls findCircleCenter() until the drone has found the circle center countmax times in a row.
+	 * then proceeds to fly straight through the circle.
+	 * @param circle
+	 */
 	public void flythroughCircle(Circle[] circle){		
 	if (circle != null){
-//		System.out.println("fandt en cirkel");
-//			for(Circle c : circle){
 				findCircleCenter(circle[0]);
 					if(CircleInCenter(circle[0])){
 						count++;
@@ -297,12 +417,16 @@ public class DroneCommander implements CircleListener{
 				}
 				else{
 				}		
-//			}	
 		}
 	}
 	
 	
-
+	/**
+	 * CirclesUpdated method comes from the implementation of circlelistener
+	 * this method is called every time a circle is found in CircleScanner
+	 * if the boolean findcircle is true
+	 * this method calls flythroughCircle
+	 */
 	@Override
 	public void circlesUpdated(Circle[] circle) {
 		if(findCircle){
